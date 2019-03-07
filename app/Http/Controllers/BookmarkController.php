42cf7d60f25179a9825f58c0a1ba175d22ce2a95
sdebarun <debarun@itobuz.com>
2019-03-07 15:47:04 +0530

    work updated

--- app/Http/Controllers/BookmarkController.php
+++ app/Http/Controllers/BookmarkController.php
@@ -18,11 +18,16 @@ class BookmarkController extends Controller
     }
 
     public function doaddmark(Request $request){
+        $data["status"] = 0;
         if ($request->hasFile('bookMarkIMage')) {
             $destinationPath = public_path().'/images/';
             $files = $request->file('bookMarkIMage'); 
             $file_name = $files->getClientOriginalName(); 
-            return $files->move($destinationPath , $file_name);
+            if($files->move($destinationPath , $file_name)){
+                $data["status"] = 1;
+                return redirect('bookmarks/add')->with('status',$data["status"]);
+            }
+            
         
             // foreach ($files as $file) {//this statement will loop through all files.
             //     $file_name = $file->getClientOriginalName(); //Get file original name
