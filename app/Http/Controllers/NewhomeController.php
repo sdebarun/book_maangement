929c84fab1e0c7fca31c46c5ccd32d511d22ba62
sdebarun <debarun@itobuz.com>
2019-03-14 19:53:17 +0530

    added notification settings and did not worked. : testing phase incomplete

--- app/Http/Controllers/NewhomeController.php
+++ app/Http/Controllers/NewhomeController.php
@@ -1,9 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use mail;
 use Illuminate\Http\Request;
-
+use App\User;
+use App\Notifications\TemplateEmail;
 class NewhomeController extends Controller
 {
     function __construct(){
@@ -11,9 +12,13 @@ class NewhomeController extends Controller
     }
 
     function index(){
-
+        $user = new User();
+    $user->email = 'sdebarun04@gmail.com';  
+    $user->notify(new TemplateEmail('hello world'));
         return view('newhome');
+
     }
+
 
     
 }
